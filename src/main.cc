2edc7c94e1d57acd55e8af5596c7811b849fc852
mibrammall <mibrammall.github@gmail.com>
2023-11-11 22:08:47 +0000

    Setup surface

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,43 @@
 #include <GLFW/glfw3.h>
 #include <webgpu/webgpu.h>
-
+#include <glfw3webgpu.h>
 #include <iostream>
+#include <cassert>
+#include <vector>
+
+WGPUAdapter requestAdapter(WGPUInstance instance, WGPURequestAdapterOptions const *options)
+{
+    struct UserData
+    {
+        WGPUAdapter adapter = nullptr;
+        bool requestEnded = false;
+    };
+    UserData userData;
+
+    auto onAdapterRequestEnded = [](WGPURequestAdapterStatus status, WGPUAdapter adapter, char const *message, void *pUserData)
+    {
+        UserData &userData = *reinterpret_cast<UserData *>(pUserData);
+        if (status == WGPURequestAdapterStatus_Success)
+        {
+            userData.adapter = adapter;
+        }
+        else
+        {
+            std::cout << "Could not get WebGPU adapter: " << message << std::endl;
+        }
+        userData.requestEnded = true;
+    };
+
+    wgpuInstanceRequestAdapter(
+        instance /* equivalent of navigator.gpu */,
+        options,
+        onAdapterRequestEnded,
+        (void *)&userData);
+
+    assert(userData.requestEnded);
+
+    return userData.adapter;
+}
 
 int main()
 {
@@ -24,6 +60,9 @@ int main()
     desc.nextInChain = nullptr;
 
     WGPUInstance instance = wgpuCreateInstance(&desc);
+    WGPUSurface surface = glfwGetWGPUSurface(instance, window);
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API); // NEW
 
     if (!instance)
     {
@@ -35,6 +74,31 @@ int main()
     // copied around without worrying about its size).
     std::cout << "WGPU instance: " << instance << std::endl;
 
+    std::cout << "Requesting adapter..." << std::endl;
+
+    WGPURequestAdapterOptions adapterOpts = {};
+    WGPUAdapter adapter = requestAdapter(instance, &adapterOpts);
+
+    std::vector<WGPUFeatureName> features;
+
+    // Call the function a first time with a null return address, just to get
+    // the entry count.
+    size_t featureCount = wgpuAdapterEnumerateFeatures(adapter, nullptr);
+
+    // Allocate memory (could be a new, or a malloc() if this were a C program)
+    features.resize(featureCount);
+
+    // Call the function a second time, with a non-null return address
+    wgpuAdapterEnumerateFeatures(adapter, features.data());
+
+    std::cout << "Adapter features:" << std::endl;
+    for (auto f : features)
+    {
+        std::cout << " - " << f << std::endl;
+    }
+
+    std::cout << "Got adapter: " << adapter << std::endl;
+
     while (!glfwWindowShouldClose(window))
     {
         // Check whether the user clicked on the close button (and any other
@@ -42,7 +106,9 @@ int main()
         glfwPollEvents();
     }
 
+    wgpuAdapterRelease(adapter);
     wgpuInstanceRelease(instance);
+    wgpuSurfaceRelease(surface);
 
     glfwDestroyWindow(window);
 
